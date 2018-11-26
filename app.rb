a6f2209741b9649f72c86be98d90a02db5cefadc
Панин М.А <mic@alice.ru>
2018-11-26 15:39:53 +0300

    Fix jQuery

--- app.rb
+++ app.rb
@@ -12,7 +12,45 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	@barbers = Barber.order "created_at DESC"
+
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# хеш
+	hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время' }
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	client = Client.new
+	client.name = @username
+	client.phone = @phone
+	client.datestamp = @datetime
+	client.barber = @barber
+	client.color = @color
+	client.save
+
+	erb "<h2>Спасибо, Вы записались.</h2>"
+
 end
